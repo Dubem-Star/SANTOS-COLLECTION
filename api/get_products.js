3e05d420d8f4c9d0074b1b519dfd2bf00ddcb3b9
Dubem-Star <chukwuloziechidubempaul@gmail.com>
2026-07-17 20:18:02 +0100

    new update

--- api/get_products.js
+++ api/get_products.js
@@ -4,11 +4,12 @@ const connectDB = require("./db");
 
 module.exports = async (req, res) => {
   setCors(res);
-  await connectDB();
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
+
+  await connectDB();
 
   console.log(req.body.message);
   res.json({ response: true });
